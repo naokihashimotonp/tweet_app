6e0a13a6a05ec72ae806c541c7071aa8e6c84b23
naokihashimotonp <nhashimoto@netprotections.co.jp>
2018-09-18 17:58:15 +0900

    user-photo func is deployed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params[:name], email: params[:email])
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      image_name: "default_user.jpg")
     if @user.save
       redirect_to("/users/#{@user.id}")
       flash[:notice] = "ユーザー登録が完了しました"
